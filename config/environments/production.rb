63df0f91d83422410865be044a7b17d63a990285
Krista Karpan <krista.karpan@gmail.com>
2016-03-03 07:28:57 +0000

    Added user authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
-Nomster::Application.configure do
+Nomster::Application.configure 
+config.action_mailer.default_url_options = { host: 'nomster-krista-karpan.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
